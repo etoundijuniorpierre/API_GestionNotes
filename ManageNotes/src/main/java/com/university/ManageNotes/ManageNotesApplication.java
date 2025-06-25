235af629b4db1124213aee6bdfe5934c2b36cae7
etoundijuniorpierre <etoundijunior@hotmail.com>
2025-06-25 11:48:02 +0100

    feat : add projet structure

---
+++ ManageNotes/src/main/java/com/university/ManageNotes/ManageNotesApplication.java
@@ -0,0 +1,13 @@
+package com.university.ManageNotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageNotesApplication.class, args);
+	}
+
+}
---
+++ ManageNotes/src/test/java/com/university/ManageNotes/ManageNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.ManageNotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
