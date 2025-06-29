e04e6af57cb60704d1e91781307c8767c5d29d55
Boutchouang Nathan <boutchouangelija@gmail.com>
2025-06-29 20:33:31 +0100

    rafactor Semester model

--- ManageNotes/src/main/java/com/university/ManageNotes/model/Semesters.java
+++ ManageNotes/src/main/java/com/university/ManageNotes/model/Semesters.java
@@ -1,26 +1,32 @@
 package com.university.ManageNotes.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
-import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
+@Getter
 @Setter
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
-public class Semesters extends AbstractEntity{
+public class Semesters extends AbstractEntity {
+
+    @Column(name = "name")
     private String name;
 
+    @Column(name = "startDate")
+    private LocalDate startDate;
+
+    @Column(name = "endDate")
+    private LocalDate endDate;
+
+    @Column(name = "active")
     private Boolean active;
 
-    @OneToMany(mappedBy = "semesters")
+    @OneToMany(mappedBy = "semesters", fetch = FetchType.LAZY)
     private List<Grades> grades;
 
 }
